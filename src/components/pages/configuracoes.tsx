b591da828218c4cae361957ed6b1b2061ffde221
Marcelo Henrique <dvmarcelohenrique@gmail.com>
2024-09-11 15:40:58 -0300

    Revert "Menu mobile marcelo"

--- src/components/pages/configuracoes.tsx
+++
@@ -1,5 +0,0 @@
-export const Configuracoes = () =>{
-    return (
-        <p>Configurações</p>
-    )
-}
--- src/components/pages/parceiros.tsx
+++
@@ -1,5 +0,0 @@
-export const Parceiros = () =>{
-    return (
-        <p>Parceiros</p>
-    )
-}
