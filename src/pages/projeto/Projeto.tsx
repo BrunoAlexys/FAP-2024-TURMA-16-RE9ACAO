85cddf104df79a3afa02d3d620f8d3377d002c4f
Marcelo Henrique <dvmarcelohenrique@gmail.com>
2024-09-18 11:04:53 -0300

    api fake criada roda com 'npm run db'

--- src/pages/projeto/Projeto.tsx
+++ src/pages/projeto/Projeto.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import { BotaoPadrao } from "../../components/botao-component/BotaoPadrao";
 import { Card } from "../../components/card-component/Card";
 import { Filter } from "../../components/filter-component/Filter";
 import { Search } from "../../components/search-component/Search";
 import imagem from './assets/perfil.png'
+import axios from "axios";
 
 export const Projeto = () => {
+    
+    interface CardData{
+        id: number,
+        projectName: string,
+        tipo: number,
+        children?: React.ReactNode
+    }
+
+    const [cards, setCards] = useState<CardData[]>([])
+
+    useEffect(()=>{
+        axios.get("http://localhost:3001/projects")
+        .then((response)=>{
+            setCards(response.data)
+        })
+        .catch((error)=>{
+            console.error(error)
+        })
+    }, [])
 
     return (
         <section id="dashboard" className="flex flex-col items-center gap-6">
@@ -16,51 +37,11 @@ export const Projeto = () => {
                 <BotaoPadrao nome="Criar" icone="add"></BotaoPadrao>
             </div>
             <div id="projetosContainer" className="flex flex-col gap-6 lg:flex-row lg:flex-wrap lg:justify-center">
-                <Card tipo={1} projectName="Conluído" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={2} projectName="50% concluido" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={3} projectName="Começo do Projeto" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
-                <Card tipo={4} projectName="Cor padrão" img={imagem}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, nam illum, voluptatem a ipsam minus debitis repellendus excepturi natus eveniet quia cumque optio mollitia nemo magnam libero et possimus vero.
-                </Card>
+                {cards.map((card)=>(
+                    <Card key={card.id} img={imagem} projectName={card.projectName} tipo={card.tipo}>
+                        {card.children}
+                    </Card>
+                ))}
             </div>
         </section>
     );
