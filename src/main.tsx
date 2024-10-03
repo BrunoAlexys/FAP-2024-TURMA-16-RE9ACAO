f98002ea9686f6f00e557e422a89aa815c03daaf
Bruno Moura <124813743+BrunoAlexys@users.noreply.github.com>
2024-10-03 10:56:20 -0300

    Criando a tela de login

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app";
+import { Login } from "./pages/login/Login";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            {/* <App /> */}
+            <Login />
         </BrowserRouter>
     </StrictMode>
 );
