79615b35d6a349b1daba209090c6f83796a9e8be
BrenoMoura00 <brenoluisadv@gmail.com>
2024-11-07 10:56:53 -0300

    Fazendo a tela de 404 pra mobile e tablet

--- src/pages/404notfound/NotFound.tsx
+++ src/pages/404notfound/NotFound.tsx
@@ -5,15 +5,15 @@ export const NotFound = () => {
     const navigate = useNavigate();
 
     return (
-        <div className='bg-corDeFundo h-screen w-full flex items-center justify-center space-x-6'>
-            <div>
-                <img src={erro404} alt="erro 404" />
+        <div className="bg-corDeFundo h-screen w-full flex flex-col lg:flex-row items-center justify-center lg:space-x-6 space-y-6 lg:space-y-0 p-4">
+            <div className="w-1/2 lg:w-auto">
+                <img src={erro404} alt="erro 404" className="w-full max-w-xs lg:max-w-md" />
             </div>
             <div className="flex flex-col items-start justify-center space-y-4">
-                <h1 className="text-9xl font-bold">404</h1>
-                <h3 className="text-3xl italic font-light mb-2">Página não encontrada!</h3>
+                <h1 className="text-9xl md:text-9xl font-bold">404</h1>
+                <h3 className="text-xl md:text-2xl lg:text-3xl italic font-light mb-2">Página não encontrada!</h3>
                 <button
-                    className="rounded-md py-2 px-4 bg-color404 font-semibold text-xl text-white"
+                    className="rounded-xl py-2 px-4 bg-color404 font-semibold text-lg md:text-xl text-white"
                     onClick={() => navigate('/')}
                 >
                     Voltar
