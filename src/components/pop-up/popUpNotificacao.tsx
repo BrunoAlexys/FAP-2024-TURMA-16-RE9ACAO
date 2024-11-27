e9db29266154005013a3e338b28172cea8836c0b
Bruno Moura <124813743+BrunoAlexys@users.noreply.github.com>
2024-11-27 15:44:11 -0300

    Resolvendo os bugs

--- src/components/pop-up/popUpNotificacao.tsx
+++ src/components/pop-up/popUpNotificacao.tsx
@@ -10,15 +10,14 @@ type NotificationPopupProps = {
 export const NotificationPopup: React.FC<NotificationPopupProps> = ({
     onClose,
     titulo,
-    time,
     descricao,
     imagem
 }) => {
-    return(
+    return (
         <div className="fixed inset-0 bg-black bg-opacity-85 flex items-center justify-center z-50">
             <div className="w-[700px] h-fit rounded-lg bg-[#D9D9D9]">
                 <div className="flex p-4 bg-gradient-to-b from-colorMenuPrimary to-colorMenuSecondary rounded-t-lg">
-                    <img src={!imagem ? imagem: PersonDefault} alt="" className="rounded-full bg-white w-24 h-24"/>
+                    <img src={!imagem ? imagem : PersonDefault} alt="" className="rounded-full bg-white w-24 h-24" />
                     <p className="font-bold text-2xl text-white p-7">{titulo}</p>
                 </div>
                 <div className="w-full flex flex-col ">
@@ -30,7 +29,7 @@ export const NotificationPopup: React.FC<NotificationPopupProps> = ({
                             className="w-36 h-10 p-4 flex items-center justify-center rounded-lg bg-colorMenuPrimary text-white font-bold"
                             onClick={onClose}
                         >
-                        Voltar 
+                            Voltar
                         </button>
                     </div>
                 </div>
--- src/pages/recuperacao-senha/nova-senha/Nova-Senha.tsx
+++ src/pages/recuperacao-senha/nova-senha/Nova-Senha.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Logo from './assets/logo.png';
 import { DiagonalSection } from '../../../components/diagonal-section/DiagonalSection';
 import { useNavigate } from 'react-router-dom';
